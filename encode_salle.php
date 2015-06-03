1230ac03735b2568e32b9bbac37d3a411c94b987
dcordier <diane.cordier@epfedu.fr>
2015-06-03 15:50:40 +0200

    nouveauencode_salle.php et test_salle.html par Diane

--- encode_salle.php
+++ encode_salle.php
@@ -5,31 +5,16 @@
 	//Sélectionner la table SALLE dans la BDD
 		$sql_info = "SELECT * FROM salle";
 		$donnees = mysqli_query($bdd, $sql_info) or die('Erreur requête SQL!<br/>'.mysqli_error($bdd));
-		//printf ("Select a retourné un fichier avec %d lignes.<br/>", mysqli_num_rows($donnees));
+		//echo mysqli_num_rows($donnees);
 		
-		$tab_salle = array();
+		$salle_info = array();
 		
 	//On range les données d'une salle dans un tableau
-		while($ligne = mysqli_fetch_object($donnees)){
-			$tab_salle[] = $ligne;
-			
+		while($d=mysqli_fetch_object($donnees)) {
+			$salle_info[] = $d;
 		}
-		//printf (count($salle_info));
 		
-		
-		//foreach ($salle_info as $s){
-			//echo $s;
-			//echo '<br/>';
-			
-		//}
-		
-		
-		$encode_salle = json_encode($tab_salle);
-		echo $encode_salle;
-		
-		
-		//var_dump(json_encode($tab_salle));
-		
+		echo json_encode($salle_info);
 	
 
 	//Fermeture de la connexion à la BDD
