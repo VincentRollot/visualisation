2a78d9de531f58c5fbad3ba08035206037f836a0
MarieAnneClay <marie-anne.clay@epfedu.fr>
2015-06-04 11:24:45 +0200

    CSS

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Gym Suédoise</title>
+    <link rel="icon" type="image/gif" href="http://design.gymsuedoise.com/USER20150427/favicon.gif" /> <!-- favicon -->
+    <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
     <link rel="stylesheet" type="text/css" href="libs/bootstrap.min.css" />
     <link rel="stylesheet" type="text/css" href="libs/jquery-ui.min.css" /> 
     <link rel="stylesheet" type="text/css" href="libs/font-awesome.min.css" />
@@ -18,7 +20,7 @@
     <div class="container">
       <div class="row">
         <div class="col-md-offset-4 col-md-4" id="form">
-          <form role="form" method="POST" action="mainPage.php">
+          <form role="form" method="POST" action="accueil.php">
             <div class="form-group">
               <label for="identify">Identifiant</label>
               <input type="email" class="form-control" id="identify" placeholder="Identifiant">
@@ -27,14 +29,11 @@
               <label for="password">Mot de passe</label>
               <input type="password" class="form-control" id="password" placeholder="*******">
             </div>
-            <button type="submit" class="btn btn-default" href="//mainPage.php">Connexion</button>
+            <button type="submit" class="btn btn-default" href="//accueil.php">Connexion</button>
           </form>
         </div>
       </div>
     </div>
-
-    <footer class="col-md-12">
-    </footer>
 
   </body>
 
