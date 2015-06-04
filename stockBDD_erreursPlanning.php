6f405fcd104f4f1c18325128b838c1a1439c6601
kouinou <aquinn.scotland@gmail.com>
2015-06-04 14:47:12 +0200

    Json resolution remodifie

---
+++ stockBDD_erreursPlanning.php
@@ -0,0 +1,9 @@
+<?php
+
+	echo "Ouverture de stockBDD_erreursPlanning.php<br/>";
+	//Connexion à la BDD
+		require 'connexion.php';
+
+	//Récupération du JSON Résolution
+	$json_resolution = file_get_contents("");
+?>
